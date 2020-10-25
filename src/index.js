fc40cd89fa65693a998762bafd9cfea2aff9aac9
Maksim Ivankou <fahrenheit9110@gmail.com>
2020-10-25 17:56:29 +0300

    init: start Braackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  if (str.length % 2 !== 0) {
+      return false
+  }
+
+  for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] == bracketsConfig[j][0]) {
+              stack.push(str[i])
+              if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                  if (stack[stack.length - 1] == stack[stack.length - 2]) {
+                      stack.pop()
+                      stack.pop()
+                  }
+              }
+          }
+          if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+              reversed = stack;
+              if (reversed[reversed.length - 1] == bracketsConfig[j][0]) {
+                  reversed.pop()
+              }
+              stack = reversed;
+          }
+      }
+  }
+  if (stack.length == 0) {
+      return true
+  } else {
+      return false
+  }
+// your solution
+
 }
